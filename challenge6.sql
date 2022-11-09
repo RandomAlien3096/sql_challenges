8987d3bd0f51c03da6027f2f11f7356300476206
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-11-09 12:59:30 -0600

    Update challenge6.sql

--- challenge6.sql
+++ challenge6.sql
@@ -25,3 +25,34 @@ employees for less than 10 months. Sort your result by ascending employee_id.
 
 Select name from Employee
 Where salary > 2000 and months < 10
+
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) 
+in STATION that is less than 137.2345 . Round your answer to 4 decimal places.
+*/
+
+Select TOP(1) CAST(ROUND(LONG_W, 4) AS DECIMAL(8,4))
+From STATION
+Where LAT_N < 137.2345
+Order by LAT_N desc
+
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. 
+Round your answer to 4 decimal places.
+*/
+
+Select top(1) CAST(ROUND(LAT_N,4) AS DECIMAL(7,4))
+FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N ASC
+
+/*
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in 
+STATION is greater than 38.7780. Round your answer to 4 decimal places.
+*/
+
+Select top(1) CAST(ROUND(LONG_W,4) AS DECIMAL(8,4))
+FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N ASC
