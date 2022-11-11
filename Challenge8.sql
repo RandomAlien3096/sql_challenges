5123f64301f21d0fa55a0cd659d7a75d065668b4
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-11-11 14:21:07 -0600

    Update Challenge8.sql

--- Challenge8.sql
+++ Challenge8.sql
@@ -28,8 +28,6 @@ FROM(
 You are given a table, BST, containing two columns: N and P, 
 where N represents the value of a node in Binary Tree, and P is the parent of N.
 
-
-
 Write a query to find the node type of Binary Tree ordered by the value of the node. 
 Output one of the following for each node:
 
@@ -48,6 +46,7 @@ FROM BST B
 ORDER BY N
 
 
+
 /*
 Samantha was tasked with calculating the average monthly salaries for all employees 
 in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after 
@@ -62,6 +61,7 @@ Select CEILING(AVG(SALARY) - AVG(CAST(REPLACE(SALARY, '0', '')AS INT))) + 1
 FROM EMPLOYEES
 
 
+
 /*
 We define an employee's total earnings to be their monthly salary*months worked, and the maximum total 
 earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find 
@@ -73,3 +73,36 @@ Select top(1) months*salary, count(*)
 from Employee
 Group by months*salary
 Order by months*salary desc
+
+
+
+/*
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+*/
+
+Select CAST(ROUND(SUM(LAT_N),2) AS DECIMAL(7,2)), CAST(ROUND(SUM(LONG_W),4) AS DECIMAL(7,2))
+FROM STATION
+
+
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater 
+than 38.7880 and less than 137.2345. Truncate your answer to  decimal places.
+*/
+
+SELECT CAST(ROUND(SUM(LAT_N), 4) AS DECIMAL(9,4))
+FROM STATION
+WHERE LAT_N >38.7880 and LAT_N <137.2345
+
+
+
+/*
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that 
+is less than 137.2345. Truncate your answer to 4 decimal places.
+*/
+
+Select cast(max(LAT_N) AS DECIMAL(9,4))
+FROM STATION
+WHERE LAT_N < 137.2345
