9f6122164b94e6fedefeac5b6d2f6354d1b61158
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-11-11 14:35:05 -0600

    Update Challenge8.sql

--- Challenge8.sql
+++ Challenge8.sql
@@ -106,3 +106,18 @@ is less than 137.2345. Truncate your answer to 4 decimal places.
 Select cast(max(LAT_N) AS DECIMAL(9,4))
 FROM STATION
 WHERE LAT_N < 137.2345
+
+
+
+/*
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+*/
+
+SELECT CAST(ABS(min(LAT_N)-max(LAT_N)) + ABS(min(LONG_W)-max(LONG_W)) AS DECIMAL(9,4))
+FROM STATION
