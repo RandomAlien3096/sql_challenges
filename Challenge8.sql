4b4cda98e7afbc4d832b641ec8a2533986444df5
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-11-11 14:10:42 -0600

    Update Challenge8.sql

--- Challenge8.sql
+++ Challenge8.sql
@@ -60,3 +60,16 @@ and round it up to the next integer.
 
 Select CEILING(AVG(SALARY) - AVG(CAST(REPLACE(SALARY, '0', '')AS INT))) + 1 
 FROM EMPLOYEES
+
+
+/*
+We define an employee's total earnings to be their monthly salary*months worked, and the maximum total 
+earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find 
+the maximum total earnings for all employees as well as the total number of employees who have maximum 
+total earnings. Then print these values as 2 space-separated integers.
+*/
+
+Select top(1) months*salary, count(*)
+from Employee
+Group by months*salary
+Order by months*salary desc
