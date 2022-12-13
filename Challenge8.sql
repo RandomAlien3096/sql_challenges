3973997c7a3965a85222cf9d2435f4047010312a
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-12-13 11:21:07 -0700

    Update Challenge8.sql

--- Challenge8.sql
+++ Challenge8.sql
@@ -204,3 +204,44 @@ count(distinct e.Senior_Manager_Code), count(distinct e.Manager_Code),
 count(distinct e.employee_Code) 
 FROM Company c 
 JOIN Employee e ON c.Company_Code = e.Company_Code GROUP BY c.Company_Code, c.Founder ORDER BY c.Company_Code
+
+
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective
+average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+Select Cn.Continent, AVG(Ct.Population)
+From Country cn
+Join City ct ON Ct.CountryCode = CN.Code
+Group By Cn.Continent
+
+
+/*
+You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+
+Grades contains the following data:
+
+Ketty gives Eve a task to generate a report containing three columns: 
+Name, Grade and Mark. 
+Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be 
+in descending order by grade -- i.e. higher grades are entered first. If there is more than one student 
+with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. 
+Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in 
+descending order. If there is more than one student with the same grade (1-7) assigned to them, order those 
+particular students by their marks in ascending order.
+
+Write a query to help Eve.
+*/
+
+SELECT 
+CASE 
+    WHEN grd.grade < 8 THEN NULL 
+    WHEN grd.grade >= 8 THEN std.name 
+    END,
+grd.grade, std.marks 
+FROM students std, grades grd
+WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
+ORDER BY grd.grade DESC, std.name ASC;
